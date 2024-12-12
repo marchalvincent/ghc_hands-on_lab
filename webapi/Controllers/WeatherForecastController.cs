c2e8338a58016dd9dc9f8910431bdab9cd922341
Vincent Marchal <vincent.marchal@avanade.com>
2024-12-12 10:39:40 +0100

    Init repo

---
+++ webapi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+using application;
+using domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return WeatherForecastManager.Get();
+        }
+    }
+}
